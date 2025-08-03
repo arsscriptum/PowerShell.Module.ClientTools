f305802fc26cb9ff7a599eb1a57658a6e9b04bdd
Guillaume Plante <guillaume.plante@icloud.com>
2025-08-02 21:42:22 -0400

    auto

---
+++ PowerShell.Module.ClientTools.psm1
@@ -0,0 +1,130 @@
+<#
+  ã--------------------------------------------------------------------------------------
+  ¦   PowerShell PowerShell.Module.ClientTools Module
+  ¦   Version 1.0.441 , Generated on Sat, 02 Aug 2025 21:40:53 GMT 
+  ¦   Description: 
+  ¦   Current Git Revision 676b38112b9b684031084bb1d23bce5741820ab6
+  L--------------------------------------------------------------------------------------
+ #>
+
+
+# ------------------------------------
+# Script file - Aliases - 
+# ------------------------------------
+$ScriptBlockAliases = "H4sIAAAAAAAACgMAAAAAAAAAAAA="
+
+# ------------------------------------
+# Script file - Config - 
+# ------------------------------------
+$ScriptBlockConfig = "H4sIAAAAAAAACs1YW2/iOBR+r9T/YEWRIGoTVZq3IqRlIZ1BU1pUpl2tdiqUAQPeBpt1nHaYbv/7Hl9CnEspvc1OHlpwzvnOzd/xMbOUTgRhFH3Ewu/GBFPxhbE4uUww73I8he8kivs9dI/29xA8f3WX0xiL3wmdEjpvjtLVinGRjBYsjadDziY4SbxrLbuKeLRs6s9KdygXsMC8OYjoNBKMr9vuLIoTfIiuojjFJ5wth2SFY0Jx2xU8hRefcLwaAGo0x23n/BbzO04ERmSGVhwn4KCT2VM2RoKDY9duf6oXvf09/cHt4VmUxkKGhtrlgK2XFHw0KjIFP0DYsSTHg/Pe5Wk4vhyFF2Mb1LEs3XYmE5ZSUWcoeydj4WRqbJFZ09Lz7hHHIuV0q+lM2kEPGkT/NZra+/09eLe/N3uk0J3V6o3q7L1PmjvD4f+UZWP5DZJc9eZ1eTYJuMDzYSQW1egHbJrGGF6TRPC1lNlEn2v5+B/kOMgDV8SCszvkUCYQoWiptJ1WFmLroS7hjUZldRNcG33BifAzBxS1ka+sOpl9B/khhZeowYzaeIpvx5GGatSVK8P37nO+V3fB663W1jm3Uy726B2L/cs0z5+8187w3asKiXyt5UAoDmrouEx5Zb3eDPt5W6U2d7Krdeaw9vJ9gjbdV0OewU6BUjXd8DuepNJ0l1GBv4tgBKrwdSQigb1ASxtV7QN0YemQr79ZeAHqpolgS6SXAh16MXKlZILexpDCIYDu34sdmgO7keMjESjzqJ4U6kRTz4YcL6OGy25AuLoN7R3oTHWSxmnmE7w155/yCtNbwhldgrHr42MAC/OFq4iT6FuMm41eeNK5PP0yto83OT2cdQZh49AgHjbkv4ZXrCW7qRby4/sUMt++z8+lmTOaUr4v8BLwV5iLdQ2pFSsa5cQCn0POGe/oEPtzyjhQoyxmdTVl0qcYuTSNY9XVsqTpPfJgCUNZjrdV4RGgJ9VamZVMQ0I8Sb0R5tDSfh3GaX/q+faJJUJtq5/GuYWxKLmWWX8936BiV+GFZNsG88WMy+pn5+QldHNHE05W4niw7tNbNomkrWCw7rLlEooZSKa8ONPGx+dwMkv8I1zMXlu0MkbqyWNeGoKgHdioa/RMLhql3YioJuJK+uBYnpH55uB3KwJ9OmN8qeojBR6pgiXVMkiLKJFF++0eVbdP8algaikDtKXd12uXpKow5amnHqYkVedNZc6u9aYsVUWq/NhR61BZqopTvkvX45SkDEzGowsGg3G5Plu3RWCrGjQtNFonknyScrujlVULiH2aiCiOi5C7IVqqBUjVAMrPbpBS1WDpdvYirFy14Fcpbc/xS8WIHoDTGvAPeefyZfNHjv8WD/TNGepF/OYCTy26B7DTQpouMZencdND/6ITxsNosvDPv/2NJ3DBsC7O8jx0m+44+IzXHowT7k0sV26CU0znEHtL3b5h0Y8F+nAEv5W4UzKbofaHI+Qr6RaCyJsuaR+1XKLEtARgkYMD2UHByAH8VIAakI6sG5cS4t5ANLQSUEVMeqqObG8T/Z84jtld4c7+zqne4TZnH4hvcKHTp1bpFqfu0/k5tf1KDffuzaz4iushOOKDhaYF4Xn3RUBnyO4wHy2gLkY3sPLjZE6YecJxw7Or43M+jyj5ofjz6XP38qsFWXfF1IPGDoWwT876ebc+41A7iyZvm/9qDbaXoFABS1n3LJPHJ2a5fDKzAOxGbg1qUJOyAccLeoRD84D5Ph8LFVQ35RxybvVecObJiVKpFliy+1Yo5CA/3SCCC5yw+BZnA2Ylyq9BAJE8kot8o8iBKRfITwP7ZJFBbhbrhO2jYyOcZ7SoUj252jWZLepUhwXQqU7iRa/saSD3arNYp1LcJdWk5jrW/s6IWju/ysc0bNgik4WVbGiGd374fYJXUriHRUTiBLlj5Ms3QIcJXI+MrYf/AKziLmu4GgAA"
+
+# ------------------------------------
+# Script file - Exception - 
+# ------------------------------------
+$ScriptBlockException = "H4sIAAAAAAAACs1XbW/bNhD+HiD/4eBqsANESvO1Wwq4tpN5TWzPUjsUWbAw0tnWKpEaSc01svz3HfViSY7itFiLzglgWeQ9z73zuEi5r0PBYYJreySlkHP0hQwOD+4PD356cXgA9HHcD5PpzB27+U/zmaNOJVfAONSkQNz9ib6GhZCQKoS7DVgzdxAHEWrHW0mx9lDGIWc65MtM7vCgoBiO3MF8PPPG08k3ZJn15/2rkTea53hXqBRbYkXorRDi/CXoFdMQoPJleIeKfiLgPrBx0MQZB6AF3KHRMXv0BVdapqS5wTpPo2jza8qicBFiUCBAIkWCUm9ALCpGkJndzlPMA6ZxKeSmzh8qoH8D0dgC61Xor0gr1WpbG4XH5BL1NPN5O0MRD7ZkIVlYUJgFP4sJrJkCY1YWEFpGXrNN+H4qJQbOFlIkJiNZVCkzfefN3nm19HM3SmPsXDFOkYqRa6efahETgeBOI40LhMnUG9Xk69k0iJhSYMNK60S9OjkJhK+cOPSlUGKhHV/EJ8jtVNGC5qhPWBKeqJw/rvhZxZ/ZlYesxvjJx8yu/8KHJUiF25r2gmcGfk2j8lA62vDoqryKinjxOtfoOlfnTcgDWuwd3RSvhyFbcqF0SFoMRIB9Cu9Ghcpx0ySRVHBFJfa1poxMNfa6M/edQncl0iiYSeHT+rmQrqY8HqwYXxL8edG7VPcYut3jyie/pMS0CP1McziDbpZZi7LV0TOnL2WwwC/AnO5W2Wmqk1R7mwR715+baDeldMIki3uVLtcz8wLJZT0CCZimIiz3ZusFg0uG8+UNWPXGRDY9t3McNDd9Jt0eg8pWUTKUv1uVaceZuXm7IIjH3eOoxLG26UwxMudPvgts4/oJWVGWeVU7dl8uU8N2GVKjafiqxKxi0th7VqM7rny3a2ObwnXMZzR9Nk92LXhC3dJFMjsAG9sODx7MavNvm9sjzu4itLceG6JmYaQ8/KTfiE/3OepvMtRoX4oldPL9te5UCEAh0cklXNT2eybDbLOdWdyl2lw/wdMF2/Wp48NFJO5YBPlk0c9VHFMnkITynkUpwinYVNc+FmZtDRmG6sssKQS+oykvnzDlghj3mxEuer2Lb6XXlEebI7DxLzg9Kvh2cmDSf3M5Ghb+qWWdJ9OirB4wUlgJN/w+duviZHod4pyR4K5DPJo+nvHIzkFijgkhtWqcB2Vfy1pe/tij5rKj3z6yzv/H/XRWT9922r29v6gbftdyUxO0dJxQx8qMG5rTzk4pQ6krwQ/BUW0bN+aeQccaTd6/8jBOfjeSVULU1TXtb0y9DuwZ0yu47WTSt53a7p0tVubN7I3pl1TaNByZgwmabnO1SOAfoOPXntBw/GXstO1rK1ABFi6mgcJf1dxr8uNRVMD6g3KDVmhW8T82glOzY46x+Bv3OfLxjjxMeY8Bm06DVNLtZ78Lm4XXqnB7xbUV1/4x48yiyw1+4bBRTE9W/n38HMPCtJMGhVqHFJQba8fjR43IWXRVJFa6fZkLCCX6/cuHW35/+lD42qIrWBQoMyTkijitt7PjcnXrQscT+ThWNh4rX7pSy7Nek9VelDTl1kzdsxLTpbtYorN3nmTmFKlS5mcanaFzy69H8/l0fgP2a+hQkxKU55chz3MCl1KkPBiIiAb/IZMf5xj8CI9RKg1v+S3vtMt+wCgS6213rLxba2ENVNumcdr/SBdLGqZt+wnYC4lYu7s01MrgW+QuJKM63c+KPLDtJ43JWJuV/N36fl6CxchKeNW0UsWlyIytqg//Ak31wcqlEQAA"
+
+# ------------------------------------
+# Script file - Helpers - 
+# ------------------------------------
+$ScriptBlockHelpers = "H4sIAAAAAAAACsVXbW7jNhD9v8DeYaAKkFRYrgMs+pEgQF3H2Q1aJ0bkbls4RqBItE2sJKokFa9h5Aa9QW/Xk3QoUrZky9nt/kgVO5HI4ZuZN49DZV5kkaQsg984lcQfc7bgRIh3JMkJ37x+BXhNB2mcEPkTzWKaLVxvpofzkIep6+kHydfGWl107tpBxGkuTydMhkkgSS7AJ39Cz9twIguePe2sm67B72NEj1SuocKopiZUJgT8QIayEAezI/yGC5wfEx6RTA5YmmPYBNxtMCqO6yJ9IBy+ATgM0YOv4aTXMzk9RaGMlpv9QN8xIcH656+/oQZ36MECfw4XIf/wB0kStnoGpVypKToM6SiM/RgmcH4suU/l1hbIPm/Kw1H3wZKt/OHHiORKPhdEhjTB+O+xPDiDJYo+GBZfv8KP+plXWpsQIf1L87SpEBvXV80Lrm+Gv49vbietigyKPGdcCvRcJDHqIUIpNGW6C3w6VgOYH3dHYRaHkvH1uS15QTpjJqgK6bznzab9hIbCdTIH7wPJ0c/MvsaFnf8GdbKDSutQIxYXCdFYXomm7+1bIrCs9gRR2raWLtp7wh+YIGApKqGMC6YGc2Y1NqKfMem+JdLHuqYYJVilORZ2yDnjfV2Sq0XGOPE82IBccrYCK6VCYKSwrZrOHuacpZCWnsB4tOBpl8Czu2Y6MqgXJCdZTLJoPQMMJatkdkvi9pX2vQVHt4Lh7DJMhCHNtBfda0oDlCEcqnAbByXiZZSoi/3Z9bYHWBe1V5TVj64TYCVvyYIKNHsfJgVxOs41WR2M6WfC/X6eDzjBFCVFenDmgkR8nUtfK9GpNYRLxodhtHTtOdCs5tmrxVNXVWMfqyJa9hyLmYI1ZivCgyXWqasl0r1BnvtXludtmuWCM3jgJPxwVpWsVr0jUip1WyriCyWAEzUdDFj2SLi8RF37A0lxJ33OudfaBapdj5H0OrDfEwDUsWqOqXMLveM2rFBLpKtMfvtmZpdRmNZQRqvmEIqo8ZlNchYt0YVz8sN3Pb93gh/o9U7Lj7NTVm0FHo9FIhU/5dpuP44DErEsFq525jW50vamcbdwNWH+YPJyTF1leSGHKvImXbsEdxbHaRs+R5uxprUCqNAqmO2Iq/aaSj3IQ5Q8nnMc+6+G9ofYXWuheF19+mqmmwybEpcEtyrx14x+VH6+mOEtpx0oe4JCHdOcJDRDguv8t0mwcr+lU/15wJ6SVTUvaamkOL1AmpX97PQ0yElE5+ufMVS3xrXTAecXFoVJ1XHM/sx1kzyEbai0ime3tpU6FGZlCS/N3JYCu7r7X8gzFXS3UYBvXLXp8YBI9aawT6F9jS8E5+WUAm0ouYV4ZX740let1seOOxUris19ZptXGXXGaGD1z0NtsMrL+HPdCsjr4l2VJB5daSiF602/n3ndW5InYURcB/C8u3dU4sbtjj6TWlRw1SJUb3QXiFw++BoOrDVe/mjkx/H93XK59O9SvPw7IYS11zIrnOOJI+YlTYh6i9plP6IZTcMET0Xdtm0Rqy7ziRxP3pTC08b4uxvkCZWu03W8aW9vildcnCIXvuOdqWnF8da1YeJwhY8rzIJSb1WuAt/Rnv4F8Wlr4joOAAA="
+
+# ------------------------------------
+# Script file - Initialize - 
+# ------------------------------------
+$ScriptBlockInitialize = "H4sIAAAAAAAACs1UXUvDMBR9H+w/XEahLayivu9hbn4U51bWyhhjSDC3MxiTkqQMlf13U2P3AaJUFLx5aJPee84955Lmpbg3TAq4FUwwwwhnLxgNOENhMim5vpG05PjaboGNxeCJcjRnTFAmVkFaFoVURqcPsuQ0UfIetQ6XLrcgijwF7v29NqkO0KAKboigxEj13POMKrGbSG2ppegdh8tFapTFXnoJ0XotFXUIIbinl6E2tieEHngXhGtst9wXlkMAgZekZ7IUdEumjwZSGMKEvsbnwJ89EBPnfgjRRH2bXHH5oQ340F/FTDGD0ZXUBvzF164twYcohyFRj1OkEI3lGNcjJvBTtE52nmYwGY/mnbpqjpzLdZ1bxa7ywInM2lgbUcUUV0xbQVG/KAYKqe3LNqkhiikEl2j2e73VqHY58dB6Ux0Jawn4d1ZBPQm3cw7+jOogpTGTQlMq4fabWu2fKW0O30Rdu7Wxs3Qrr+9g/L9uYN19txnEyR7ERK1i2rD+dK++XzB7E3e/gN+d+VbhH07eWbBHsNVUrc0b+4ZP5YAFAAA="
+
+# ------------------------------------
+# Script file - ModuleUpdater - 
+# ------------------------------------
+$ScriptBlockModuleUpdater = "H4sIAAAAAAAACs1VW0/bMBR+R+I/eFGmphJJ6eskpDFYp6KVVRTYw9QHN3FImBNHvgyhwn/fsZ2Lk5Zt7GFa+uKcfufzuX5JVRnLnJXoE5HhGc1JKa8Zo+KWcAHmJZYZ2h4eIHj8BUvM+wkKBmj4R1EyL1PGC6zpxlFjEhJTqt1qEpf4BF2wvAzNuWUf/bCIkXXgRCpe9vwOD54PD/QvbYK/JA+74dQes5ySJoVvZ0VCifyQl0le3gUrVVWMS7HKmKLJkrOYCDFeW2yFOS4Ceza+S20gkvBggcsES8YfIQM/xVSQxskANxDD2p+LUyXZTQVIonEzjTv6Gz7xkMs4W/szxmNi7eO6mi907OQX7aw9L0SLDZwuvEDoxcYutT26B1cPIujGAqp9Cbm8ZjI0viaoRDGtXjlYTiaVSBr3liq6IhXFMQlGkTaNjpA+JNNRUzjblxtOwcvLpKzEu8kEcyFinldSFdFdLjO1iXI22R4/eyhM3USb6ivOz2177yBu0+nwZmZiRd5b4fVnHm5bSQ6D597J8UN9lxKEx6yUkHsUs8KNRscw4SQVk4zgREwKLGBuJjVvBOXZH2GeoiB4E1wTIW1/vbbv3niMQsZRYMcK3rbOyFUiVkKygm3uSSzXPoOQh8b3joN+dDUg9jooneM0Oo6O6yI0z2cIvV0Kry3hANWbKc/Ja4DrtbFr6gDV1d/Ahu0YoPtr667xIBMWY7pcLaaGtJk8by/ovAElQxBIWXP0Qca0WukWQU2wvp6hJ3TGStBEWIhQ/9PhterNJSnQTm+RsV8/VgQZ+QtNj1H48RStJKtQeIupIrsXPqEvSoaXilJ7i9HZWmwduTXz5GyprU7yb1X2/1ZFvXkvL56zOV95LkkIrBsmCPJKhoSKM5TqtrU+URQhZ2Z+97lzmmeyqofJZG8Fpjcs7YTNOCucGfNtx7pd0DzRcB10pj1kX0nqr/na7+ShpeorRhOuuRpsdl0tslvZfdQFSyztvPzBvpPwCsq+IDJjCcgxz+3KW1no3E3cTlAhlahm6mVgwrFrDNvbRtQu9n5ksYNcDJGarpYkLe/b6bP5RFklN56tnh3VhqG+O1zFLlfxx1x9tp44uULwE95SEFQtCgAA"
+
+
+
+# ------------------------------------
+# Loader
+# ------------------------------------
+function ConvertFrom-Base64CompressedScriptBlock {
+
+    [CmdletBinding()] param(
+        [String]
+        $ScriptBlock
+    )
+
+    # Take my B64 string and do a Base64 to Byte array conversion of compressed data
+    $ScriptBlockCompressed = [System.Convert]::FromBase64String($ScriptBlock)
+
+    # Then decompress script's data
+    $InputStream = New-Object System.IO.MemoryStream(, $ScriptBlockCompressed)
+    $GzipStream = New-Object System.IO.Compression.GzipStream $InputStream, ([System.IO.Compression.CompressionMode]::Decompress)
+    $StreamReader = New-Object System.IO.StreamReader($GzipStream)
+    $ScriptBlockDecompressed = $StreamReader.ReadToEnd()
+    # And close the streams
+    $GzipStream.Close()
+    $InputStream.Close()
+
+    $ScriptBlockDecompressed
+}
+
+$AssembliesFound = $False
+$AssemblyFolder = "$PSScriptRoot\assemblies"
+if(Test-Path "$AssemblyFolder" -PathType 'Container'){
+    $Assembly = @( Get-ChildItem -Path "$AssemblyFolder\*.dll" -ErrorAction SilentlyContinue )    
+    $AssemblyCount = $Assembly.Count
+    if($AssemblyCount -gt 0 ){ $AssembliesFound = $True }
+}
+
+
+if($AssembliesFound){
+    $FoundErrors = @(
+      Foreach ($Import in @($Assembly)) {
+        try {
+            Add-Type -Path $Import.Fullname -ErrorAction Stop
+        } catch [System.Reflection.ReflectionTypeLoadException] {
+            Write-Warning "Processing $($Import.Name) Exception: $($_.Exception.Message)"
+            $LoaderExceptions = $($_.Exception.LoaderExceptions) | Sort-Object -Unique
+            foreach ($E in $LoaderExceptions) {
+                Write-Warning "Processing $($Import.Name) LoaderExceptions: $($E.Message)"
+            }
+            $true
+            #Write-Error -Message "StackTrace: $($_.Exception.StackTrace)"
+        } catch {
+            Write-Warning "Processing $($Import.Name) Exception: $($_.Exception.Message)"
+            $LoaderExceptions = $($_.Exception.LoaderExceptions) | Sort-Object -Unique
+            foreach ($E in $LoaderExceptions) {
+                Write-Warning "Processing $($Import.Name) LoaderExceptions: $($E.Message)"
+            }
+            $true
+            #Write-Error -Message "StackTrace: $($_.Exception.StackTrace)"
+        }
+      }
+    )
+
+    if ($FoundErrors.Count -gt 0) {
+        $ModuleName = (Get-ChildItem $PSScriptRoot\*.psd1).BaseName
+        Write-Warning "Importing module $ModuleName failed. Fix errors before continuing."
+        break
+    }
+}
+
+# For each scripts in the module, decompress and load it.
+# Set a flag in the Script Scope so that the scripts know we are loading a module
+# so he can have a specific logic
+$Script:LoadingState = $True
+$ScriptList = @('Aliases','Config','Exception','Helpers','Initialize','ModuleUpdater')
+$ScriptList | ForEach-Object {
+    $ScriptId = $_
+     $ScriptBlock = "`$ScriptBlock$($ScriptId)" | Invoke-Expression
+    $ClearScript = ConvertFrom-Base64CompressedScriptBlock -ScriptBlock $ScriptBlock
+    try{
+        $ClearScript | Invoke-Expression
+    }catch{
+        Write-Host "===============================" -f DarkGray
+        Write-Host "$ClearScript" -f DarkGray
+        Write-Host "===============================" -f DarkGray
+        Write-Error "ERROR IN script $ScriptId . Details $_"
+    }
+}
+$Script:LoadingState = $False
+
+
+
